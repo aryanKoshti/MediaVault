5f3de5a6d94843a7f79c21b77ccd325e343d9504
Aryan Koshti <aryankosti120@gmail.com>
2026-06-26 10:50:08 +0530

    Update project

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -16,7 +16,7 @@ const CollectionCard = ({item}) => {
             <a target='_blank' className='h-full' href={item.url}>
                 {item.type == 'photo' ? <img className='h-full w-full object-cover object-center' src={item.src} alt="" /> : ''}
                 {item.type == 'video' ? <video className='h-full w-full object-cover object-center' autoPlay loop muted src={item.src}></video> : ''}
-                {item.type == 'gif' ? <img className='h-full w-full object-cover object-center' src={item.src} alt="" /> : ''}
+                {/* {item.type == 'gif' ? <img className='h-full w-full object-cover object-center' src={item.src} alt="" /> : ''} */}
             </a>
             <div id='bottom' className='flex justify-between gap-3 items-center w-full px-4 py-6 absolute bottom-0 text-white'>
                 <h2 className='text-lg font-semibold capitalize h-14 overflow-hidden'>{item.title}</h2>
